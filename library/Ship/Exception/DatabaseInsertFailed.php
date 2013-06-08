308198c42b85588a657e4964b918f18fb13896b5
Roger Thomas <rogere84@gmail.com>
2013-06-08 20:06:54 +0100

    Adds Boat service to add a new boat record.

---
+++ library/Ship/Exception/DatabaseInsertFailed.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * GitShip.com PHP Library
+ *
+ * @copyright GitShip.com
+ *
+ * @author GitShip.com
+ * @link http://www.GitShip.com
+ *
+ * @license This software and associated documentation (the "Software") may not be
+ * used, copied, modified, distributed, published or licensed to any 3rd party
+ * without the written permission of GitShip.com
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all licensed copies of the Software.
+ *
+ */
+namespace Ship\Exception;
+
+/**
+ * DatabaseInsertFailed
+ */
+class DatabaseInsertFailed extends \Exception
+{
+}
---
+++ library/Ship/Service/Boat.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * GitShip.com PHP Library
+ *
+ * @copyright GitShip.com
+ *
+ * @author GitShip.com
+ * @link http://www.GitShip.com
+ *
+ * @license This software and associated documentation (the "Software") may not be
+ * used, copied, modified, distributed, published or licensed to any 3rd party
+ * without the written permission of GitShip.com
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all licensed copies of the Software.
+ *
+ */
+namespace Ship\Service;
+
+use Ship\Exception\DatabaseInsertFailed;
+use Ship\Service;
+
+/**
+ * Boat
+ */
+class Boat extends Service
+{
+    /**
+     * Create a boat record
+     *
+     * @param string $url
+     * @param array $tags (optional)
+     * @return array
+     * @throws \Exception
+     * @example
+     *     $service = new \Ship\Service\Boat();
+     *     $service->addBoat(
+     *         'http://25.media.tumblr.com/tumblr_lod3zgOWsu1qmlz9do1_500.jpg',
+     *         array(
+     *             'boat',
+     *             'rubber duck',
+     *             'duck',
+     *             'yellow'
+     *         )
+     *     );
+     */
+    public static function addBoat($url, $tags)
+    {
+        if (!is_array($tags)) {
+            throw new \Exception('tags was expected to be an array.');
+        }
+
+        $now = new \MongoDate(time());
+
+        $boat = array();
+        $boat['cd'] = new \MongoDate(time());
+        $boat['u'] = $url;
+        $boat['t'] = $tags;
+
+        $collection = static::getDatabaseConnection()->boats;
+        /* @var $collection \MongoCollection */
+        $insert = $collection->insert(
+            $boat,
+            array(
+                "safe" => true
+            )
+        );
+
+        if (\Ship\Validate\Mongo::isValidInsertStatic($insert)) {
+            return $boat;
+        }
+
+        throw new DatabaseInsertFailed('Insert to Mongo failed');
+    }
+}
