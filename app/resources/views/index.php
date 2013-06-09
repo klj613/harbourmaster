5e74b4faf8618033703cc48c7ca4391903908a29
Roger Thomas <rogere84@gmail.com>
2013-06-09 13:20:23 +0100

    Flat UI layout changes

--- app/resources/views/index.php
+++ app/resources/views/index.php
@@ -1,7 +1,30 @@
 <?php include_once APPLICATION_PATH . '/resources/view-elements/meta-head.php'; ?>
-    <body>
+<body>
         <?php include_once APPLICATION_PATH . '/resources/view-elements/header.php'; ?>
-       	Home
+
+        <div class="container">
+
+            <div class="row">
+                <div class="span12">
+                    <input type="text" value="" placeholder="boat tags..." class="span12">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="span6">
+                    <a href="#" class="btn btn-medium btn-block btn-inverse get-boat">
+                        Get My Boat
+                    </a>
+                </div>
+                <div class="span6">
+                    <a href="#" class="btn btn-medium btn-block btn-info random-boat">
+                        Random Boat
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- /container -->
+
         <?php include_once APPLICATION_PATH . '/resources/view-elements/footer.php'; ?>
-	</body>
+    </body>
 </html>
