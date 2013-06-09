b2e0049d22bc13dc4a85a35cbe492665422e0f1f
Roger Thomas <rogere84@gmail.com>
2013-06-09 18:37:38 +0100

    Removes JS imports that aren't used.

--- app/resources/view-elements/footer.php
+++ app/resources/view-elements/footer.php
@@ -17,9 +17,13 @@
     <script src="/js/bootstrap.min.js"></script>
     <script src="/js/bootstrap-select.js"></script>
     <script src="/js/bootstrap-switch.js"></script>
+    <script src="/js/jquery.tagsinput.js"></script>
+<?php
+/**
     <script src="/js/flatui-checkbox.js"></script>
     <script src="/js/flatui-radio.js"></script>
-    <script src="/js/jquery.tagsinput.js"></script>
     <script src="/js/jquery.placeholder.js"></script>
     <script src="/js/jquery.stacktable.js"></script>
+ */
+?>
     <script src="/js/application.js"></script>
