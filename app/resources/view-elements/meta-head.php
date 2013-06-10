ebf34c92a30a9118752be4fc33d788aa7856478e
Roger Thomas <rogere84@gmail.com>
2013-06-10 20:34:29 +0100

    Adds fork link and <title> meta

--- app/resources/view-elements/meta-head.php
+++ app/resources/view-elements/meta-head.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Harbour Master</title>
+        <title>GitShip.com - Keep on shippin'</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
         <link href="/styles/flat-ui.css" rel="stylesheet">
